0a90edca0897bf4289fdf4a46668442ff2c327d6
chros73 <chros73@gmail.com>
2021-09-19 07:57:36 +0100

    Add other and config settings

--- bscpylgtv/webos_client.py
+++ bscpylgtv/webos_client.py
@@ -1386,18 +1386,49 @@ class WebOsClient:
 
         return await self.luna_request(uri, params)
 
-    async def set_pc_mode(self, pc_mode=True, hdmi_input="hdmi2"):
-        """Set PC mode for an HDMI input."""
+    async def set_other_settings(self, settings):
+        """Set other settings.
 
-        inputs = ["hdmi1", "hdmi2", "hdmi3", "hdmi4"]
+        A possible list of settings and example values are below (not all settings are applicable
+        for all modes and/or tv models):
 
-        if hdmi_input not in inputs:
-            return
+        "gameOptimizationHDMI1": False,
+        "gameOptimizationHDMI2": False,
+        "gameOptimizationHDMI3": False,
+        "gameOptimizationHDMI4": False,
+        "hdmiPcMode": {
+            "hdmi1": False,
+            "hdmi2": False,
+            "hdmi3": False,
+            "hdmi4": False
+        },
+        "uhdDeepColorHDMI1": False,
+        "uhdDeepColorHDMI2": False,
+        "uhdDeepColorHDMI3": False,
+        "uhdDeepColorHDMI4": False
+
+        """
 
         uri = "com.webos.settingsservice/setSystemSettings"
 
-        # {"category": "other", "settings": {"hdmiPcMode": {"hdmi1" : False, "hdmi2": True, "hdmi3" : False, "hdmi4" : False} }}
-        params = {"category": "other", "settings": {"hdmiPcMode": {hdmi_input: pc_mode} }}
+        params = {"category": "other", "settings": settings}
+
+        return await self.luna_request(uri, params)
+
+    async def set_configs(self, settings):
+        """Set config settings.
+
+        Example:
+
+        "com.palm.app.settings.enableHdmiPcLabel": False,
+        "tv.model.motionProMode": "OLED Motion",
+        "tv.model.motionProMode": "OLED Motion Pro"
+
+        """
+
+        uri = "com.webos.service.config/setConfigs"
+
+        params = {"configs": settings}
 
         return await self.luna_request(uri, params)
 
